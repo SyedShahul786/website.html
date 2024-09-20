3fc978c57fd0ef321855330aac988f6640aba946
SyedShahul786 <syedshahul070920@gmail.com>
2024-09-20 11:20:34 +0530

    Add files via upload

---
+++ web_project2/web3/script.js
@@ -0,0 +1,28 @@
+
+        // $(document).ready(function() {
+        //     // Load stored data on page load
+        //     loadStoredData();
+
+        //     // Save data when button is clicked
+        //     $('#save').click(function() {
+        //         const club_name = $('#name').val();
+        //         const Paid_amount = $('#paid').val();
+        //         const Membership_duration = $('#membership').val();
+
+        //         // Store data in local storage
+        //         localStorage.setItem('name', club_name);
+        //         localStorage.setItem('Paid_amount', Paid_amount);
+        //         localStorage.setItem('Membership_duration', Membership_duration);
+
+        //         // Load stored data
+        //         loadStoredData();
+        //     });
+
+        //     function loadStoredData() {
+        //         const Club_Name = localStorage.getItem('club_name') || 'No name stored';
+        //         const Paid_amount = localStorage.getItem('Paid_amount') || 'No paid amount stored';
+        //         const Membership_duration = localStorage.getItem('Membership_duration')||'No membership';
+        //         $('#storedData').html(`<strong>Name:</strong> ${Club_Name}<br><strong>Email:</strong> ${Paid_amount}<br><strong>Membership_duration:</strong> ${Membership_duration}`);
+        //     }
+        // });
+    
